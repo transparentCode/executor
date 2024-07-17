8c0ef67308d5b58a0c9255d382ffa655d1510466
transparentCode <priyanshugupta221@gmail.com>
2024-07-17 11:35:39 +0530

    adds configuration for component/bean scanning

---
+++ src/main/java/com/virtual/executor/config/VirtualThreadConfig.java
@@ -0,0 +1,9 @@
+package com.virtual.executor.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.virtual.executor")
+public class VirtualThreadConfig {
+}
